8e66f5e0f2c03f3e17a5293badfc271cd0beb552
Antony Jones <antony@beyonk.com>
2020-09-25 18:36:34 +0100

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<style lang="scss">
+	h1 {
+		color: blue;
+	}
+</style>
+
+
+<TextField validate={() => {}} validity={writable({})} />
+<TextArea validate={() => {}} validity={writable({})} />
+
+<script>
+	import { writable } from 'svelte/store'
+	import { TextField, TextArea } from '@beyonk/fields'
+</script>
